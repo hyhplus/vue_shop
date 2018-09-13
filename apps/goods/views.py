f661afbdeaacc243fb3de185b2ea4442613440f7
hyhplus <2859268670@qq.com>
2018-09-14 00:43:51 +0800

    update 0911 xadmin后台管理

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -4,8 +4,8 @@ from rest_framework.views import APIView
 from .serializers import GoodsSerializer
 from rest_framework.response import Response
 from rest_framework import status
-
-# Create your views here.
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 # django rest framework:  DRF
 
 """
@@ -17,14 +17,14 @@ pip install coreapi
 pip install django-guardian
 """
 
-class GoodsListView(APIView):
-    """
-    商品列表
-    """
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        goods_serializer = GoodsSerializer(goods, many=True)
-        return Response(goods_serializer.data)
+# class GoodsListView(APIView):
+#     """
+#     商品列表页
+#     """
+#     def get(self, request, format=None):
+#         goods = Goods.objects.all()[:10]
+#         goods_serializer = GoodsSerializer(goods, many=True)
+#         return Response(goods_serializer.data)
 
     # def post(self, request, format=None):
     #     serializer = GoodsSerializer(data=request.data) # drf封装get/post/body的请求的数据
@@ -32,3 +32,31 @@ class GoodsListView(APIView):
     #         serializer.save()
     #         return Response(serializer.data, status=status.HTTP_201_CREATED)
     #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GoodsPagination(PageNumberPagination):
+    """
+    商品列表分页
+    """
+    page_size = 10
+    page_size_query_param = 'page_size'
+    page_query_param = 'p'
+    max_page_size = 100
+
+
+class GoodsListView(generics.ListAPIView):
+    """
+    商品列表页
+    """
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
+    pagination_class = GoodsPagination
+
+
+
+
+
+
+
+
+
+
